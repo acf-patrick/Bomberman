920d8631c35d90cd2b6e86da169b19f3557fe06f
TsilaAllaoui <ratsilakwel@gmail.com>
2020-11-04 10:51:09 +0300

    added pocs for stage start ane main loop for playing stage

--- object.h
+++ object.h
@@ -16,7 +16,6 @@ public:
 	GameObject() {}
 	virtual ~GameObject() {}
 
-	/* servira à mettre à jour l'objet à chaque tour de boucle */
 	virtual void update();
 	virtual void draw(SDL_Surface*);
 
@@ -25,13 +24,10 @@ public:
 	Vector<float> getPosition();
 	void setPosition(float, float);
 
-	/* getter anlay attribut groups */
 	std::vector<Group*> getOwner();
 
-	/* vérifie si l'objet appartient à un groupe */
 	bool isAlive();
 
-	/* Enlève l'objet de tout groupe */
 	void kill();
 
 friend class Group;

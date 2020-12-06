50a1281650f7b5d435e783bc711a29e830b52ee4
acf-patrick <miharisoap@gmail.com>
2020-12-06 23:55:17 +0300

    Explosion : implement draw method

--- explosion.cpp
+++ explosion.cpp
@@ -42,62 +42,62 @@ Explosion::Explosion(int x, int y, int portee):
 			std::cerr << "erreur lors de la création de l'éxplosion" << std::endl;
 			continue;
 		}
-		for (int j=0; j<3; ++j)
+
+		// center
+		SDL_Rect src = { 0, Sint16(i*PX), PX, PX }, dest = { Sint16((size.x-PX)*0.5), Sint16((size.y-PX)*0.5) };
+		SDL_BlitSurface(spritesheet, &src, surface[i], &dest);
+
+		// top
+		src.x = Sint16((i+1)*PX);
+		src.y = 0;
+		dest.y = 0;
+		SDL_BlitSurface(spritesheet, &src, surface[i], &dest);
+
+		// vertical
+		src.y += PX;
+		for (int k=0; k<=2*range; ++k)
 		{
-			// center
-			SDL_Rect src = { 0, Sint16(j*PX), PX, PX }, dest = { Sint16((size.x-PX)*0.5), Sint16((size.y-PX)*0.5) };
-			SDL_BlitSurface(spritesheet, &src, surface[i], &dest);
-
-			// top
-            src.x = Sint16((j+1)*PX);
-            src.y = 0;
-            dest.y = 0;
-            SDL_BlitSurface(spritesheet, &src, surface[i], &dest);
-
-            // vertical
-			src.y += PX;
-            for (int k=0; k<=2*range; ++k)
-			{
-				dest.y += PX;
-				// avoid center
-				if (k == range+1)
-					SDL_BlitSurface(spritesheet, &src, surface[i], &dest);
-			}
-
-            // bottom
-			src.y += PX;
-			dest.y = Sint16(size.y - PX);
-            SDL_BlitSurface(spritesheet, &src, surface[i], &dest);
-
-            // left
-			src.x = 0;
-            src.y = Sint16((j+3)*PX);
-            dest.x = 0;
-            dest.y = Sint16((size.y-PX)*0.5);
-            SDL_BlitSurface(spritesheet, &src, surface[i], &dest);
-
-            // horizontal
-			src.x += PX;
-            for (int k=0; k<=2*range; ++k)
-			{
-				dest.x += PX;
-				// avoid center
-				if (k == range+1)
-					SDL_BlitSurface(spritesheet, &src, surface[i], &dest);
-			}
-
-			// right
-			src.x += PX;
-            dest.x = Sint16((size.x-PX)*0.5);
-            SDL_BlitSurface(spritesheet, &src, surface[i], &dest);
+			dest.y += PX;
+			// avoid center
+			if (k != range)
+				SDL_BlitSurface(spritesheet, &src, surface[i], &dest);
 		}
+
+		// bottom
+		src.y += PX;
+		dest.y = Sint16(size.y - PX);
+		SDL_BlitSurface(spritesheet, &src, surface[i], &dest);
+
+		// left
+		src.x = 0;
+		src.y = Sint16((i+3)*PX);
+		dest.x = 0;
+		dest.y = Sint16((size.y-PX)*0.5);
+		SDL_BlitSurface(spritesheet, &src, surface[i], &dest);
+
+		// horizontal
+		src.x += PX;
+		for (int k=0; k<=2*range; ++k)
+		{
+			dest.x += PX;
+			// avoid center
+			if (k != range)
+				SDL_BlitSurface(spritesheet, &src, surface[i], &dest);
+		}
+
+		// right
+		src.x += PX;
+		dest.x = Sint16(size.x-PX);
+		SDL_BlitSurface(spritesheet, &src, surface[i], &dest);
 		SDL_SetColorKey(surface[i], SDL_SRCCOLORKEY, 0xff00ffff);
 	}
+
+	timer.start();
 }
 
 void Explosion::draw()
 {
-    Vector<int> p(Renderer::camera->convert(position.x - (size.x+PX)*0.5, position.y - (size.y+PX)*0.5));
+    Vector<int> p(Renderer::camera->convert(position.x - (size.x-PX)*0.5, position.y - (size.y-PX)*0.5));
     SDL_Rect pos = { Sint16(p.x), Sint16(p.y) };
     SDL_BlitSurface(surface[state], NULL, Renderer::screen, &pos);
 }
@@ -109,7 +109,7 @@ void Explosion::update()
 
 	if (timer.get_elapsed_time() >= dt)
 	{
-        if (state == 3)
+        if (state == 2)
             sens *= -1;
         state += sens;
 		timer.start();
